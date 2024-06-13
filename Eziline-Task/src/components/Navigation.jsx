13e30d2db7a6a279b23600fdecce58807e4b0e19
Ibadurrehman143 <ibadurrehman010@gmail.com>
2024-06-13 10:53:52 +0500

    Eziline

---
+++ Eziline-Task/src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+import '../App.css';
+const Navigation = () =>{
+    return (
+        <nav>
+        <div className="className">
+          <h1>ArchLab.</h1>
+        </div>
+        <ul>
+          <li><a href="Home">Home</a></li>
+          <li><a href="About">About</a></li>
+          <li><a href="Team">Team</a></li>
+          <li><a href="Service">Service</a></li>
+          <li><a href="Project">Project</a></li>
+          <li><a href="Blog">Blog</a></li>
+          <li><a href="Contact">Contact</a></li> 
+        </ul>
+      </nav>
+    )
+}
+export default Navigation
